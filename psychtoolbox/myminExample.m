df77501e8cb98b5716e03adb54e94e7711337378
beresluca <71323525+beresluca@users.noreply.github.com>
2021-01-21 11:15:16 +0100

    audio + timestamps added to video

--- psychtoolbox/myminExample.m
+++ psychtoolbox/myminExample.m
@@ -1,6 +1,6 @@
 % Minimal example to reproduce the file saving problem with custom Gstreamer input
 
-% Displays and records video for "vidLength" secs from camera at /dev/video0 
+% Displays and records video for "vidLength" secs (or untill keypress) from camera at /dev/video0 
 % using a custom gsteramer pipe. Display is at top-left corner, 
 % all params are hardcoded.
 
@@ -9,10 +9,8 @@
 %% Basic params
 
 moviename = 'mytest.mov';
-
 withsound = 2; % record with sound
 windowed = 1;
-
 PsychDefaultSetup(1);
 vidLength = 900;  % maximum length for video in secs
 Screen('Preference', 'Verbosity', 6);
@@ -22,7 +20,7 @@ screen=max(Screen('Screens'));
 RestrictKeysForKbCheck(KbName('ESCAPE'));
 
 % Custom Gstreamer pipeline definition:
-capturebinspec = 'v4l2src device=/dev/video0 ! jpegdec ! videoconvert' % sound? alsasrc \ ! audio/x-raw,width=16,depth=16,rate=44100,channel=1 \ ! audioconvert \';
+capturebinspec = 'v4l2src device=/dev/video0 ! jpegdec ! videoconvert'
 
 try
     Screen('SetVideoCaptureParameter', -1, sprintf('SetNextCaptureBinSpec=%s', capturebinspec));
@@ -35,18 +33,18 @@ end
 codec = ':CodecType=DEFAULTencoder';
 codec = [moviename, codec];
 
-vidstartAt = GetSecs + 20; % starting time for video in secs?
+vidstartAt = GetSecs + 15; % video capture delay in secs
 
 %% Open device + Start capture
 try
     % Init a window in top-left corner, skip tests
     oldsynclevel = Screen('Preference', 'SkipSyncTests', 1);
-    win = Screen('OpenWindow', screen, 0, [0 0 1000 600]);
-    Screen('Flip',win);
+    win = Screen('OpenWindow', screen, 0, [0 0 1400 850]);
+    Screen('Flip', win);
     Screen('TextSize', win, 24);
     
     % Open video capture device
-    grabber = Screen('OpenVideoCapture', win, -9, [0 0 1920 1080], [], [], [], codec, withsound, [], 8);
+    grabber = Screen('OpenVideoCapture', win, -9, [0 0 1280 720], [], [], [], codec, withsound, [], 8);
     
     KbReleaseWait;
     WaitSecs('YieldSecs', 2);
@@ -55,30 +53,45 @@ try
     oldtex = 0;
     count = 0;
     
-    % Start capture with target fps
+    % preallocate frame info holding vars
+    frameCaptTime = nan(vidLength*30, 1);
+    droppedFrames = nan(vidLength*30, 1);
+    
+    % Start capture with 30 fps
     [Fps, startTime] = Screen('StartVideoCapture', grabber, 30, 1, vidstartAt);
 
-    % startTime = vidstartAt;
     % Run until keypress or until maximum allowed time is reached
     while ~KbCheck && GetSecs < vidstartAt+vidLength
         
         % Wait blocking for next image then return it as texture
-        [tex, captureTimestamp, ~] = Screen('GetCapturedImage', win, grabber, 1, oldtex);
+        [tex, pts, nrdropped] = Screen('GetCapturedImage', win, grabber, 1, oldtex);
 
-        if tex > 0
-            Screen('DrawTexture', win, tex);  % Draw new texture from device
-            oldtex = tex;  % Recycle texture
-            Screen('Flip', win);  % Show new texture
-            count = count + 1;  
-        else
-            WaitSecs('YieldSecs', 0.005);
-        end
-        
+           % If a texture is available, draw and show it.
+            if tex > 0
+                % Print capture timestamp in seconds since start of capture:
+                Screen('DrawText', win, sprintf('Capture time (secs): %.4f', pts), 0, 0, 255);
+
+                % Draw new texture from framegrabber.
+                Screen('DrawTexture', win, tex);
+                oldtex = tex;
+                Screen('Flip', win);
+                
+                count = count + 1;
+                
+                % Store frame-specific values
+                frameCaptTime(count, 1) = pts;
+                droppedFrames(count, 1) = nrdropped;
+                
+            else  % if tex
+                WaitSecs('YieldSecs', 0.005);
+                
+            end  % if tex
+      
     end  % while
 
     % Done, report elapsed time
     telapsed = GetSecs - startTime;
-    disp([char(10), 'Elapsed time: ', num2str(telapsed), ' secs']);  
+    
     % Shutdown
     Screen('StopVideoCapture', grabber);  % Stop capture engine and recording  
     Screen('CloseVideoCapture', grabber);  % Close engine and recorded movie file
@@ -95,8 +108,11 @@ catch ME
     
 end  % try
 
-disp([char(10), num2str(startTime), ...
-      char(10), num2str(captureTimestamp)]);
+% report start time of capture, elapsed time 
+disp([char(10), 'Start of capture: ', num2str(startTime)]);
+disp([char(10), 'diff: ', num2str(startTime - vidstartAt)]);
+disp([char(10), 'Elapsed time: ', num2str(telapsed), ' secs']); 
+##      char(10), num2str(frameCaptTime)]);
 
 RestrictKeysForKbCheck([]);
 
