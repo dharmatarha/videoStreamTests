af5011f6872222e57788b25afe3bd9c488fc0343
beresluca <71323525+beresluca@users.noreply.github.com>
2021-01-27 15:14:10 +0100

    fixed sound playback, still no recording
    used chunks of 'DelayedSoundFeedbackDemo'

--- psychtoolbox/synced_videorecording_v2.m
+++ psychtoolbox/synced_videorecording_v2.m
@@ -8,7 +8,7 @@ pkg load sockets;
 remoteIP = '10.160.12.108';
 
 moviename = 'mytest1.mov';
-withsound = 2; % record with sound
+withsound = 0; % record with sound
 windowed = 1;
 PsychDefaultSetup(1);
 vidLength = 900;  % maximum length for video in secs
@@ -17,6 +17,10 @@ screen=max(Screen('Screens'));
 
 % Perform basic initialization of the sound driver:
 InitializePsychSound;
+channels = 2;
+%channels = [2,1]; %separate channel for recording? --> 'audiodata' matrix should match in size!!
+%selectchannels = [0, 6; 12, 14];
+freq = 48000;
 
 
 function sharedStartTime = handshake(remoteIP)
@@ -186,15 +190,134 @@ try
     [Fps, vidcaptureStartTime] = Screen('StartVideoCapture', grabber, 30, 1, vidstartAt);
     
     % open & start audio feedback     
-    pa = PsychPortAudio('Open', [], 4+2+1, [], [], 2);   
-    %pa = PsychPortAudio('Open', [], 3, [], [], 2); 
-    diResult = PsychPortAudio('DirectInputMonitoring', pa, 1, -1, 0, 0.0, 0.5);
+    %pa = PsychPortAudio('Open', [], 4+2+1, [], [], 2);   
+    painput = PsychPortAudio('Open', [], 2+1, 1, [], channels, [], [], selectchannels); % under 'channels' optionally we can define a 2 element vector specifying different channels for input / output
+    paoutput = painput;
     
-    if diResult > 0
-        % Failed! Need to use our fallback implementation:    
-        fprintf('Full-duplex monitoring mode active.\n');
-        PsychPortAudio('Start', pa, 0, 0, 1); % sharedStartTime ?? 
+    % Preallocate an internal audio recording  buffer with a capacity of at least
+    % 10 seconds, possibly more if requested lat'ency is higher:
+    lat = 150/1000;
+    PsychPortAudio('GetAudioData', painput, max(2 * lat, 10));
+      
+    % Allocate a zero-filled (ie. silence) output audio buffer of more than
+    % sufficient size: Three times the requested latency, but at least 30 seconds.
+    % One could do this more clever, but this is a safe no-brainer and memory
+    % is cheap:
+    outbuffersize = floor(freq * 3 * max(lat, 10));
+    PsychPortAudio('FillBuffer', paoutput, zeros(2, outbuffersize));
     
+    playbackstart = PsychPortAudio('Start', paoutput, 0, sharedStartTime, 1); % sharedStartTime ?? 
+    
+    % Wait until at least captureQuantum seconds of sound are available from the capture
+    % device and then quickly fetch it from the capture device. captureQuantum
+    % is the minimum amount of sound data that the driver can capture. If you'd
+    % ask for less you'd get at least this amount anyway + possibly extra
+    % delays:
+    s = PsychPortAudio('GetStatus', painput);
+    headroom = 1;
+    headroom = round(headroom);
+    captureQuantum = headroom * (s.BufferSize / s.SampleRate);
+##    if verbose > 1
+##      fprintf('CaptureQuantum (Duty cycle length) is %f msecs, for a buffersize of %i samples.\n', captureQuantum * 1000, s.BufferSize);
+##    end
+    
+    [audiodata offset overflow capturestart] = PsychPortAudio('GetAudioData', painput, [], captureQuantum);
+    
+    % Sanity check returned values: audiodata should be at least headroom * s.BufferSize
+    % samples, offset should be zero as this is the first 'GetAudioData' call
+    % since 'Start' of capture. overflow should be zero, otherwise we screwed
+    % up our timing already in the first few milliseconds because the system is
+    % not up to the task / overloaded for the requested latency settings.
+    % 'capturestart' contains the estimated time when the first returned audio
+    % sample hit the microphone / line-in connector:
+    if (size(audiodata, 2) < headroom * s.BufferSize) || (offset~=0) || (overflow > 0)
+      fprintf('WARNING: SOUND ONSET TIMING SCREWED!! THE SYSTEM IS NOT UP TO THE TASK/OVERLOADED!\n');
+      fprintf('Realsize samples %i < Expected size %i? Or offset %i ~= 0 ? Or overflow %i > 0 ?\n', size(audiodata, 2), headroom * s.BufferSize, offset, overflow);
+      timingfailed = 1;
+    end
+    
+    % Ok, we have our initial batch of audio samples in 'audiodata', recorded
+    % at time 'capturestart'. The sound output is currently feeding zeroes
+    % (=silence) from the zero-filled output buffer to the speakers and the
+    % first zero-sample in that buffer will hit the speakers at time
+    % 'playbackstart'. We now need to copy our 'audiodata' batch of samples
+    % into the output buffer, but at an offset from the start that is selected
+    % to exactly achieve output of our first 'audiodata' sample at the
+    % requested latency.
+    %
+    % The first sample was captured at time 'capturestart' and the requested
+    % latency for output is 'lat': Therefore the wanted playback time for this
+    % first sample is...
+    reqonsettime = capturestart + lat;
+    
+    % Sanity check: Are we ahead of the playback stream with our requested
+    % onset time of reqonsettime? If not, then the system won't be able to
+    % achieve the requested 'lat'ency and we'll be late!
+    s = PsychPortAudio('GetStatus', paoutput);
+    if s.CurrentStreamTime > reqonsettime
+      fprintf('WARNING: SOUND ONSET TIMING SCREWED!! THE SYSTEM IS NOT UP TO THE TASK/OVERLOADED!\n');
+      fprintf(['Requested onset at time %f seconds, but audio stream is already at time %f seconds\n--> ' ...
+      'We will be at least %f msecs too late!\n'], reqonsettime, s.CurrentStreamTime, 1000 * (s.CurrentStreamTime - reqonsettime));
+      timingfailed = 2;
+    end
+    
+    % The first sample from the output buffer will playback at time
+    % 'playbackstart', therefore our first sample should be placed at a
+    % timeoffset relative to the start of the outputbuffer of...
+    reqtimeoffset = reqonsettime - playbackstart;
+    
+    % Our first audio sample needs to be placed at a time offset of
+    % 'reqtimeoffset' in the audio output buffer, overwriting the "silence"
+    % there. Map offset in seconds to offset in samples: The system plays out
+    % s.SampleRate samples per second, so we need to place our audio at an
+    % offset of...
+    reqsampleoffset = round(reqtimeoffset * s.SampleRate);
+    
+    if reqsampleoffset < 0
+      fprintf('If sound feedback works at all, then extra latency will be at least %f msecs, probably more!\n', 1000 * abs(reqtimeoffset));    
+    end
+    
+    % Make sure the offset is positive, ie at least zero:
+    reqsampleoffset = max(reqsampleoffset, 0);
+    
+    % Overwrite the output buffer with our captured audiodata, starting at
+    % sample index 'reqsampleoffset'. Need to set the 'streamingrefill' flag to
+    % 1 in order to enable this special overwrite mode. The 'underflow' flag
+    % will tell us if we made the refill in time, or if we "missed the train"
+    % in the last microsecond: A non-zero value means we missed.
+    [underflow, nextSampleStartIndex, nextSampleETASecs] = PsychPortAudio('FillBuffer', paoutput, audiodata, 1, reqsampleoffset);
+    
+    s = PsychPortAudio('GetStatus', paoutput);
+    if underflow > 0
+      fprintf('WARNING: SOUND ONSET TIMING SCREWED!! THE SYSTEM IS NOT UP TO THE TASK/OVERLOADED!\n');
+      fprintf(['Requested onset at time %f seconds, but audio stream is already at time %f seconds\n--> ' ...
+      'We will lose at least the first %f msecs of the sound signal!\n'], reqonsettime, s.CurrentStreamTime, 1000 * (s.CurrentStreamTime - reqonsettime));
+      timingfailed = 3;
+    end
+    
+    % Ok, if we made it until here without a non-zero 'timingfailed' flag, then
+    % at least the first few milliseconds of captured sound should play at
+    % exactly the desired 'lat'ency between capture and playback.
+    
+    % From now on we'll just need to periodically fetch chunks of audio data
+    % from the capture device and feed it into the output device without any
+    % complex math or tricks involved. However in order to avoid dropouts and
+    % other audible artifacts we need to make sure that we feed new data fast
+    % enough. We will now execute a loop that tries to fetch audio in the
+    % smallest possible quantity from the capturedevice, then immediately
+    % append it to the output buffer:
+    updateQuantum = s.BufferSize / s.SampleRate;
+    
+    % Get current status of outputdevice:
+    s1 = PsychPortAudio('GetStatus', paoutput);
+    
+    oldcaptureQuantum = -1;
+    cumoverrun   = 0;
+    cumunderflow = 0;
+
+        
+    
+    temp = GetSecs;
     
     % Run until keypress or until maximum allowed time is reached
         while ~KbCheck && GetSecs < vidstartAt+vidLength
@@ -226,21 +349,66 @@ try
                     
                 end  % if tex
              
-            % audio playback    
-            s=PsychPortAudio('GetStatus', pa);
-            disp(s);
-                if s.CaptureStartTime > 0
-                   fprintf('Estimated minimal roundtrip latency is %f msecs.\n', 1000 * (s.StartTime - s.CaptureStartTime));
+            % audio playback status
+                if temp + 5
+                    s=PsychPortAudio('GetStatus', paoutput);
+                    %disp(s);
                 end
+                
+            captureQuantum = updateQuantum;
+
+              if captureQuantum ~= oldcaptureQuantum
+                oldcaptureQuantum = captureQuantum;
+##                if verbose > 1
+##                  fprintf('Duty cycle adapted to %f msecs...\n', 1000 * captureQuantum);
+##                end
+              end
+              
+              % Get new captured sound data ...
+              fetchDelay = GetSecs;
+              [audiodata, offset, overrun] = PsychPortAudio('GetAudioData', painput, [], captureQuantum);
+              fetchDelay = GetSecs - fetchDelay;
+              underflow = 0;
+              
+              % ... and stream it into our output buffer:
+              while size(audiodata, 2) > 0
+                % Make sure to never push more data in the buffer than it can
+                % actually hold, ie not more than half its maximum capacity:
+                fetch = min(size(audiodata, 2), floor(outbuffersize / 2));
+                % We feed data in chunks of 'fetch' samples:
+                pushdata = audiodata(:, 1:fetch);
+                % audiodata is the remainder which will be pushed in the next loop
+                % iteration:
+                audiodata = audiodata(:, fetch+1:end);
+                
+                % Perform streaming buffer refill. As long as we don't push more
+                % than a buffer size, the driver will take care of the rest...
+                [curunderflow, nextSampleStartIndex, nextSampleETASecs] = PsychPortAudio('FillBuffer', paoutput, pushdata, 1);
+                underflow = underflow + curunderflow;
+              end
+              
+              % Check for xrun conditions from low-level sound hardware:
+              s1 = PsychPortAudio('GetStatus', paoutput);
+              s2 = PsychPortAudio('GetStatus', painput);
+              xruns = s1.XRuns + s2.XRuns;
+                          
+              cumoverrun = cumoverrun + overrun;
+              cumunderflow = cumunderflow + underflow;
+
+              
+              % Done. Next iteration...
               
               
         end  % while
-
-    end 
+      
+   
   
     % User wants us to finish. Disable input monitoring:
-    PsychPortAudio('DirectInputMonitoring', pa, 0);
-    PsychPortAudio('Stop', pa);
+    %PsychPortAudio('DirectInputMonitoring', paoutput, 0);
+    PsychPortAudio('Stop', paoutput);
+    PsychPortAudio('Stop', painput);
+    % Drain its capture buffer...
+    PsychPortAudio('GetAudioData', painput);
     % Done - Close device and driver:
     PsychPortAudio('Close');
       
@@ -253,7 +421,6 @@ try
     vidStopTime = GetSecs; 
     Screen('CloseVideoCapture', grabber);  % Close engine and recorded movie file
     vidCloseTime = GetSecs; 
-    % tmp = isequal(vidStopTime, vidCloseTime);
     
     sca;
     % Report fps
