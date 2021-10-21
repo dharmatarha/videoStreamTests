f6acf299b11e89f0060270c96bf9f41ec60fabeb
dharmatarha <adam.boncz@gmail.com>
2021-10-21 19:29:00 +0200

    local changes from gondor machine - set of small fixes after testing

--- freeConv.sh
+++ freeConv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # USAGE: ./freeConv.sh "pairNo" "labName"
-# positional argments should be "pairNo" (int) and "labName" (str, Gondor pr Mordor)
+# positional argments should be "pairNo" (int) and "labName" (str, Gondor or Mordor)
 
 echo "Input arg pairNo: "$1
 echo "Input arg labName: "$2
@@ -17,7 +17,7 @@ PATH=~/CommGame/videoStreamTests:$PATH
 if [ "$2" == "Mordor" ]; then
   gstCommand="gst-launch-1.0 -v v4l2src device=/dev/video0 ! image/jpeg,width=1920,height=1080,framerate=30/1 ! jpegdec ! queue ! videoconvert ! rtpvrawpay ! udpsink host=192.168.1.60 port=19009 sync=false"
 elif [ "$2" == "Gondor" ]; then
-   gstCommand="gst-launch-1.0 -v v4l2src device=/dev/video0 ! image/jpeg,width=1920,height=1080,framerate=30/1 ! jpegdec ! queue ! videoconvert ! rtpvrawpay ! udpsink host=192.168.1.1 port=19009 sync=false" 
+   gstCommand="gst-launch-1.0 -v v4l2src device=/dev/video1 ! image/jpeg,width=1920,height=1080,framerate=30/1 ! jpegdec ! queue ! videoconvert ! rtpvrawpay ! udpsink host=192.168.1.1 port=19009 sync=false" 
 fi
    
 STREAMLOG=$1"_"$2"_camStreamLog.txt"
