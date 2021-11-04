96e60766ea1a0f5a0978ba40b44a8763ce6f6d8e
Peter Velosy <peter@petervelosy.com>
2021-11-04 05:54:55 +0100

    Added result dir checks and proper input arg checks

--- timingTest.sh
+++ timingTest.sh
@@ -1,28 +1,52 @@
 #!/bin/bash
 #
-# USAGE: ./timingTest.sh LABNAME
+# USAGE: ./timingTest.sh PAIRNO LABNAME
 #
 # Script testing the system clock drift and network transmission time across local and remote machines.
 #
 # Uses SSH to run a simple python3 function on both ends, estimates timings from its output.
 #
 # Inputs:
+# PAIRNO 	- int, 1:99
 # LABNAME 	- str, "Mordor" or "Gondor"
 #
 # 
 
-echo -e "\nInput arg LABNAME: "$1
+
+echo -e "\nInput arg PAIRNO: "$1
+echo "Input arg LABNAME: "$2
 
 # check for input args
-if [[ $# -eq 0 ]] ; then
-    echo "Input arg LABNAME is required (Mordor/Gondor)"
+if [[ $# -ne 2 ]] ; then
+    echo "Input args PAIRNO and LABNAME are required!"
     exit 1
 fi
+if (( $2 > 0 && $2 < 100)) ; then
+    PAIRNO=$2
+else
+    echo "Input arg PAIRNO should be integer between 1 and 99!"
+    exit 2
+fi    
 if [[ $1 == "Mordor" ]] || [[ $1 == "Gondor" ]] ; then
     LABNAME=$1
 else
     echo "Input arg LABNAME should be either Mordor or Gondor!"
+    exit 3
 fi
+
+# check for result dir for pair
+RESULTDIR="/home/mordor/CommGame/pair"$PAIRNO
+if [[ -d RESULTDIR ]] ; then
+    echo -e "\nResult folder for pair "$PAIRNO" already exists."
+else
+    MKDIR_RETVAL=$(mkdir $RESULTDIR)
+    if [[ MKDIR_RETVAL == 0 ]] ; then
+        echo -e "\nCreated results directory for pair "$PAIRNO
+    else
+        echo -e "\nFailed to create results directory at "$RESULTDIR"!"
+        exit 4
+    fi
+fi     
 
 # hardcoded expected IP value for motion PC
 MOTIONPC_IP="192.168.1.30"
@@ -41,8 +65,8 @@ fi
 
 # get current time for filenames, down to minutes
 CURRENT_TIME=$(date +%m_%d_%H_%M)
-REMOTE_TIMINGFILE="remoteTiming_"$OTHERLAB"_"$CURRENT_TIME".txt"
-LOCAL_TIMINGFILE="localTiming_"$LABNAME"_"$CURRENT_TIME".txt"
+REMOTE_TIMINGFILE=$RESULTDIR"/remoteTiming_"$OTHERLAB"_"$CURRENT_TIME".txt"
+LOCAL_TIMINGFILE=$RESULTDIR"/localTiming_"$LABNAME"_"$CURRENT_TIME".txt"
 
 # run ssh and local versions of timing tests
 echo -e "\nRunning the timing functions...\n"
